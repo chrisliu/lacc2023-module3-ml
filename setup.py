b78c01473b97fbe72aeb363e592ce4105ce63c89
Dylan Kupsh <dkupsh@ucla.edu>
2023-07-13 15:15:01 -0700

    fix for setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='LACC Module 3',
    author='Dylan Kupsh and Chris Liu',
    author_email='dkupsh@cs.ucla.edu, c',
-   package_dir = {"module3": "lacc/module3"},
+   package_dir = {"lacc": "lacc"},
    install_requires=[], #external packages as dependencies
 )
