169b3a13a6a53bc2b1ae850a604c8a194a7eebc7
Christopher Liu <chrisliu1999@gmail.com>
2023-07-13 15:45:51 -0700

    refactor

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-   name='lacc-module3',
-   version='1.0',
-   description='LACC Module 3',
-   author='Dylan Kupsh and Chris Liu',
-   author_email='dkupsh@cs.ucla.edu, c',
-   package_dir={'lacc': 'lacc'},
-   install_requires=[], #external packages as dependencies
+    name="lacc-module3",
+    version="1.0",
+    description="LACC Module 3",
+    author="Dylan Kupsh and Chris Liu",
+    author_email="dkupsh@cs.ucla.edu, c",
+    package_dir={"lacc": "lacc"},
+    install_requires=[],  # external packages as dependencies
 )
